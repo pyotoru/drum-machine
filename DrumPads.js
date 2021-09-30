6915cc6e91947267e87fd03cd9c4a9a0886c83d9
Piotr Wawrzyniak <wawrzyniak.peter@gmail.com>
2021-09-30 17:11:26 +0900

    fixed sounds on keydown

--- DrumPads.js
+++ DrumPads.js
@@ -3,27 +3,65 @@ import cat from "./svg/cat.svg";
 
 const DrumPads = ({drumpads}) => {
   const {keyTrigger, keyCode, sound, id} = drumpads;
-
   const [text, setText] = useState("");
 
+  // const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", playOnKey);
+    return () => {
+      document.removeEventListener("keydown", playOnKey);
+    };
+  }, []);
+
   const playOnKey = (e) => {
-    if (e.keyCode === keyCode) {
-      var audio = new Audio(sound);
+
+    if (e.keyCode === drumpads[0].keyCode) {
+      var audio = new Audio(drumpads[0].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[1].keyCode) {
+      var audio = new Audio(drumpads[1].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[2].keyCode) {
+      var audio = new Audio(drumpads[2].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[3].keyCode) {
+      var audio = new Audio(drumpads[3].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[4].keyCode) {
+      var audio = new Audio(drumpads[4].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[5].keyCode) {
+      var audio = new Audio(drumpads[5].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[6].keyCode) {
+      var audio = new Audio(drumpads[6].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[7].keyCode) {
+      var audio = new Audio(drumpads[7].sound);
+      audio.play();
+      setText(id);
+    }
+     if (e.keyCode === drumpads[8].keyCode) {
+      var audio = new Audio(drumpads[8].sound);
       audio.play();
       setText(id);
     }
   };
-
-  console.log(document.addEventListener("keydown", playOnKey));
-
-  // const [active, setActive] = useState(false);
-
-  // useEffect(() => {
-  //   document.addEventListener("keydown", handleKeyPress);
-  //   return () => {
-  //     document.removeEventListener("keydown", handleKeyPress);
-  //   };
-  // }, []);
 
   // const handleKeyPress = (e) => {
   //   if (e.keyCode === drumpads[0].keyCode) {
@@ -41,6 +79,16 @@ const DrumPads = ({drumpads}) => {
     <>
       {drumpads.map((drumpad) => {
         const {keyTrigger, keyCode, sound, id} = drumpad;
+
+//   const playOnKey = (e) => {
+//     if (e.keyCode === keyCode) {
+//       var audio = new Audio(sound);
+//       audio.play();
+//       setText(id);
+//     }
+//   };
+
+// document.addEventListener("keydown", playOnKey);
 
         // window.addEventListener("keydown", (e) => {
         //   if (e.keyCode === keyCode) {
